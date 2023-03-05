dec304d9745004fdec77e3f79fb307a936704ee7
csongin <cheonsong37@gmail.com>
2023-03-05 16:22:14 +0900

    miniproject 복습

--- mini-project/backend/src/cheerio-scraping.js
+++ mini-project/backend/src/cheerio-scraping.js
@@ -1,17 +1,15 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import axios from "axios";
+import cheerio from "cheerio";
 
 export async function createUserAPI(url) {
   const result = await axios.get(url);
 
   const $ = cheerio.load(result.data);
   const og = {};
-  let key = '';
-  let value = '';
-  $('meta').each((_, el) => {
-    if ($(el).attr('property')) {
-      const key = $(el).attr('property').split(':')[1];
-      const value = $(el).attr('content');
+  $("meta").each((_, el) => {
+    if ($(el).attr("property")) {
+      const key = $(el).attr("property").split(":")[1];
+      const value = $(el).attr("content");
       og[key] = value;
     }
   });
