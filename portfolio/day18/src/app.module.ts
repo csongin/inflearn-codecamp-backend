6e3cfff478d1258491be9015a78c325f4ecc5f3e
csongin <cheonsong37@gmail.com>
2023-03-11 18:23:22 +0900

    Apply environment variables

--- portfolio/day18/src/app.module.ts
+++ portfolio/day18/src/app.module.ts
@@ -1,26 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // ConfigModule 추가
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './apis/user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // ConfigModule 추가
     UserModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/commons/graphql/schema.gql',
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '172.22.0.1',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'workout',
-      entities: [__dirname + '/apis/**/*.entity.*'],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // ConfigModule 추가
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.MYSQL_HOST,
+        port: parseInt(process.env.MYSQL_PORT),
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB,
+        entities: [__dirname + '/apis/**/*.entity.*'],
+        synchronize: true,
+        logging: true,
+      }),
     }),
   ],
 })
