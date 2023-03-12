01ef705686cbbb6a581b67d840b812622787bc54
csongin <cheonsong37@gmail.com>
2023-03-12 15:20:06 +0900

    CRUD API delete complete

---
+++ portfolio/delete-join-api/src/apis/payment/entities/payment.entity.ts
@@ -0,0 +1,41 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Product } from 'src/apis/product/entities/product.entity';
+import { User } from 'src/apis/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Payment {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column({ default: false })
+  @Field(() => Boolean)
+  status: boolean;
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @Field(() => Date)
+  updatedAt: Date;
+
+  @JoinColumn()
+  @ManyToOne(() => Product)
+  @Field(() => Product)
+  product: Product;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+}
